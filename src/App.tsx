52eeca20fdf18a6fb7d08eac7cca2adfd20cfbbd
Robdea <roberto008namales@gmail.com>
2025-08-17 23:41:22 -0600

    hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { HashRouter, Route, Routes } from "react-router"
 import MainLayout from "./layouts/MainLayout"
 import Home from "./interfaces/Home"
 import Album from "./interfaces/Album"
@@ -8,14 +8,14 @@ import Album from "./interfaces/Album"
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<MainLayout/>}>
             <Route index element={<Home/>} />
             <Route path="album/:albumId" element={<Album/>} />
           </Route>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
